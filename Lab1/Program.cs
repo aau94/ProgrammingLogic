b9caff461ba5d49bbb58e8903d0fef77726b093b
aau94 <s01206465@acad.tri-c.edu>
2025-07-01 13:49:25 -0400

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -8,4 +8,13 @@ class Program
     }/*Allen Au
        IT 1050-Lab 1*/ 
 }      //Step 2
-Console.Writeline(Allen Au IT 1050) 
+    Console.WriteLine(Allen Au IT 1050) 
+       //Step 3
+    int favorite = 3;
+    Console.WriteLine(favorite); 
+    string programminglanguage = Python
+    Console.WriteLine(programminglanguage);
+    double NumProgramming = 1;
+    Console.WriteLine(NumProgramming);
+    bool programmingexperience = no
+    Console.WriteLine(programmingexperience); 
