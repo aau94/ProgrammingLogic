102ef06418e5058dff5598ae9941373251dcfa6b
aau94 <s01206465@acad.tri-c.edu>
2025-07-03 19:34:13 -0400

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         //Step 1
-    Console Write.Line(Allen Au IT 1050 Lab1) 
+        //Allen Au IT 1050 Lab1
+        //Step 2
+    Console Write.Line(Allen Au IT 1050) 
+        //Step 3
+    int favorite = 3;   
+    Console Write.Line("My favorite number is" + favorite);
+    string programminglanguage = "Python";     
+    Console.WriteLine("My favorite programming language is" + programminglanguage);   
+    double numberofprogramminglanguages = 1;
+    Console.WriteLine("The number of programs I've written before this is" + numberofprogramminglanguages);
+    bool experience = true;
+    Console.WriteLine("I have programming experience" + experience);  
+        //Step 4
+        //Step 5
+    double myDouble = 9.78;
+    int myInt = (int)myDouble;
+    bool myBool = true;
+    Console.WriteLine(Convert.ToString(myInt));
+    Console.WriteLine(Convert.ToString(myBool));
+        //Step 6
+    Console.WriteLine("Enter username");
+    string username = Console.ReadLine();
+    Console.WriteLine("Username is" + username);
+    Console.WriteLine("Enter your age:");
+    int age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Your age is:" + age);
+    Console.WriteLine("Hello" + username! + "You are age years old");    
         }
