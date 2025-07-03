16a05df374f4e1c142f18a6311169dbbdf50b2e7
aau94 <s01206465@acad.tri-c.edu>
2025-07-03 17:42:25 -0400

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,42 +5,5 @@ class Program
     static void Main(string[] args)
     {
         //Step 1
-    }/*Allen Au
-       IT 1050-Lab 1*/ 
-}      //Step 2
-    Console.WriteLine(Allen Au IT 1050) 
-       //Step 3
-    int favorite = 3;
-    Console.WriteLine("My favorite number is" favorite); 
-    string programminglanguage = Python
-    Console.WriteLine("My favorite programming language is" programminglanguage);
-    double NumProgramming = 1;
-    Console.WriteLine("The number of programs I've written is" NumProgramming);
-    bool programmingexperience = yes
-    Console.WriteLine("Do I have any experience programming?" programmingexperience); 
-      //Step 4
-    SCHOOL_NAME = "Tri-C" #Declaring the constant string
-    print(SCHOOL_NAME)    #Displaying the constant string
-      //Step 5
-    double myDouble = 9.78; 
-    int myInt = (int) myDouble; 
-    System.out.println("Converted value:" + myInt); // Output: Converted Value: 9
-    integer_value = 9
-    string_value = str(integer_value)
-    bool_value = True
-    string_value = str(bool_value)
-    print(string_value) # Output: True 
-    Console.WriteLine(double:9.78) 
-    Console.WriteLine(integer_value:9)
-    Console.WriteLine(bool_value:True)     
-        //Step 6
-    Console.WriteLine("Enter name:")
-    string Name = Console.ReadLine();
-    Console.WriteLine("Name is:" + Name); 
-    Console.WriteLine("Enter your age:");
-    int age = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Your age is:" + age);
-    print(f"Hello {name}! Your age is {age}.") 
-        //Step 7
-    int num1 = 5;
-    int num2 = 10;
+    Console Write.Line(Allen Au IT 1050 Lab1) 
+        }
